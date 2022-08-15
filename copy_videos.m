3d59060a7b8406d14b6a3fbc52fb1d74512ca8b0
SethYC <seth.campbell@uleth.ca>
2022-08-14 19:23:31 -0600

    tidy up

--- copy_videos.m
+++ copy_videos.m
@@ -1,11 +1,10 @@
-%simply trasnfers all desired reaching videos from my NAS storage to a
-%local drive to make some manual testing easier. 
+%Simply transfers all desired reaching videos from my NAS storage to a
+%local drive under one folder to make some manual inspecting easier. 
 
 root_dir = 'Y:\Seth_temp\Thesis recordings';
 video_dir = dir([root_dir, '\*\*\*cam2.avi']); %only cam2 files for the reaching view
 
 for i = 1:length(video_dir) %for all videos
-% for i = 1:2
     video_path = [video_dir(i).folder, '\', video_dir(i).name];
     copyfile (video_path, 'E:\Seth local\cohort 1 cam2 videos\')
     fprintf("progress: %i/%i\n", i , length(video_dir))
