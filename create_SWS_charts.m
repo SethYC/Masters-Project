298089bd66b4d80a3fff8b5744c2083668c97804
SethYC <seth.campbell@uleth.ca>
2022-08-16 19:30:54 -0600

    add chart creation

--- create_SWS_charts.m
+++ create_SWS_charts.m
@@ -65,10 +65,98 @@ for i = 1:length(epochs_t.rat_num) %for each epochs.mat file
 end
 
 %%
-%%create chart
+%%create plots
+
+%helper functions
+my_sem = @(x) std(x)/sqrt(size(x,1));
+
+x = reshape(epochs_t.R1_SWS_pre45,15,[])'; %format durations into a 6x15 (# of rats by 15 days) matrix
+x = circshift(x,-1,2); %move probe trial results in column 1 to last column instead and shift eveything else left
+
+y = reshape(epochs_t.R2_SWS_pre75,15,[])'; %format durations into a 6x15 (# of rats by 15 days) matrix
+y = circshift(y,-1,2); %move probe trial results in column 1 to last column instead and shift eveything else left
 
 
+%split into groups
+x_con = x(5:6,:); %pre-task pre45
+x_exp = x(1:4,:);
 
+y_con = y(5:6,:); %post-task pre75
+y_exp = y(1:4,:);
+
+%----------------------------------
+%pre-task pre45 charts
+
+%pre45 all rats
+%maybe ineffeicent way to plot each rat's pre45 dur, but works for now
+charts = tiledlayout('flow'); 
+ax1 = nexttile; hold on;
+plot(x(1,:),'-o','markersize', 5, 'DisplayName','rat 1E')
+plot(x(2,:),'-o','markersize', 5, 'DisplayName','rat 2E')
+plot(x(3,:),'-o','markersize', 5, 'DisplayName','rat 3E')
+plot(x(4,:),'-o','markersize', 5, 'DisplayName','rat 4E')
+plot(x(5,:),'-o','markersize', 5, 'DisplayName','rat 1C')
+plot(x(6,:),'-o','markersize', 5, 'DisplayName','rat 2C')
+legend
+title("pre-task sleep duration")
+
+%pre45 mean and std total
+ax2 = nexttile;
+shadedErrorBar(1:length(x),x,{@mean,my_sem},'lineprops',{'-o','LineWidth',1.5,'MarkerSize',8,'Color','g'})
+title("pre-task sleep Mean duration for all rats (SEM)")
+
+% %pre45 mean and std groups
+% nexttile; hold on;
+% shadedErrorBar(1:length(x_con),x_con,{@mean,@std},'lineprops',{'-o','LineWidth',1.5,'MarkerSize',8})
+% shadedErrorBar(1:length(x_exp),x_exp,{@mean,@std},'lineprops',{'-o','LineWidth',1.5,'MarkerSize',8})
+% legend('Control','Experimental')
+% title("pre-task sleep Group duration (STD)")
+
+%pre45 mean and sem groups
+ax3 = nexttile; hold on;
+shadedErrorBar(1:length(x_con),x_con,{@mean,my_sem},'lineprops',{'-o','LineWidth',1.5,'MarkerSize',8})
+shadedErrorBar(1:length(x_exp),x_exp,{@mean,my_sem},'lineprops',{'-o','LineWidth',1.5,'MarkerSize',8})
+legend('Control','Experimental')
+title("pre-task sleep group duration (SEM)")
+
+%-----------------------------------------
+%post-task pre75 charts
+
+%pre75 all rats
+ax4 = nexttile; hold on;
+plot(y(1,:),'-o','markersize', 5, 'DisplayName','rat 1E')
+plot(y(2,:),'-o','markersize', 5, 'DisplayName','rat 2E')
+plot(y(3,:),'-o','markersize', 5, 'DisplayName','rat 3E')
+plot(y(4,:),'-o','markersize', 5, 'DisplayName','rat 4E')
+plot(y(5,:),'-o','markersize', 5, 'DisplayName','rat 1C')
+plot(y(6,:),'-o','markersize', 5, 'DisplayName','rat 2C')
+legend
+title("post-task sleep duration")
+
+%pre75 mean and std total
+ax5 = nexttile;
+shadedErrorBar(1:length(y),y,{@mean,my_sem},'lineprops',{'-o','LineWidth',1.5,'MarkerSize',8,'Color','g'})
+title("post-task sleep Mean duration for all rats (SEM)")
+
+% %pre75 mean and std groups
+% nexttile; hold on;
+% shadedErrorBar(1:length(y_con),y_con,{@mean,@std},'lineprops',{'-o','LineWidth',1.5,'MarkerSize',8})
+% shadedErrorBar(1:length(y_exp),y_exp,{@mean,@std},'lineprops',{'-o','LineWidth',1.5,'MarkerSize',8})
+% legend('Control','Experimental')
+% title("post-task sleep Group duration (STD)")
+
+%pre75 mean and sem groups
+ax6 = nexttile; hold on;
+shadedErrorBar(1:length(y_con),y_con,{@mean,my_sem},'lineprops',{'-o','LineWidth',1.5,'MarkerSize',8})
+shadedErrorBar(1:length(y_exp),y_exp,{@mean,my_sem},'lineprops',{'-o','LineWidth',1.5,'MarkerSize',8})
+legend('Control','Experimental')
+title("post-task sleep group duration (SEM)")
+
+%full chart formatting
+title(charts,"Comparison of SWS duration")
+linkaxes([ax1,ax2,ax3,ax4,ax5,ax6],'y')
+xlabel(charts,'day')
+ylabel(charts,'duration (hh:mm:ss)')
 %%
 %%functions
 
