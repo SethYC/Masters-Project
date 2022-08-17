a03eff1ec32e7f97a4802cf0521d63582fde8a1d
SethYC <seth.campbell@uleth.ca>
2022-08-17 01:15:48 -0600

    load epochs_table instead of creating it

--- create_SWS_charts.m
+++ create_SWS_charts.m
@@ -3,34 +3,35 @@
 %%
 %%create table of epoch files and their information
 
-%load table var 't'
-load('Y:\Seth_temp\Thesis recordings\directory_table.mat') %created by create_recording_directores()
-
-%remove any file (row) from baseline phase
-% t.phase = categorical(t.phase); %convert to categorical for next step
-ix = t.phase == 'Baseline'; %index of rows with baseline
-t(ix,:) = []; %delete the rows
-
-%find unique groups of rat_num,group,phase & day (thus ignoring epoch)
-[groups, epochs_t] = findgroups(t(:,1:4)); 
-
-%add file_path column to table
-file_path = cell(length(epochs_t.rat_num),1); %init empty cell array for paths
-epochs_t = addvars(epochs_t,file_path);
-
-%convoluted way to get one of the paths from a row in t that matches a
-%group. i.e. in directoy table t (with baseline phase removed), for every unique group
-%(such as rat 1, group E, Training phase) there are three rows that
-%differ by the epoch column (i.e. post-task,pre-task and task). The last
-%column is the file_path to these three epoch locations for this group, but i
-%just want one of them as i will later be removing the last two files in
-%the path to just have the path to the epochs.mat file two folders up. 
-epochs_t.file_path = splitapply(@(x) x(1),t.path,groups);
-
-%remove last two files in each file path (thus leading to epochs.mat)
-epochs_t.file_path = cellfun(@remove_last_2_folders,epochs_t.file_path,'UniformOutput',false);
+% %load table var 't'
+% load('Y:\Seth_temp\Thesis recordings\directory_table.mat') %created by create_recording_directores()
+% 
+% %remove any file (row) from baseline phase
+% % t.phase = categorical(t.phase); %convert to categorical for next step
+% ix = t.phase == 'Baseline'; %index of rows with baseline
+% t(ix,:) = []; %delete the rows
+% 
+% %find unique groups of rat_num,group,phase & day (thus ignoring epoch)
+% [groups, epochs_t] = findgroups(t(:,1:4)); 
+% 
+% %add file_path column to table
+% file_path = cell(length(epochs_t.rat_num),1); %init empty cell array for paths
+% epochs_t = addvars(epochs_t,file_path);
+% 
+% %convoluted way to get one of the paths from a row in t that matches a
+% %group. i.e. in directoy table t (with baseline phase removed), for every unique group
+% %(such as rat 1, group E, Training phase) there are three rows that
+% %differ by the epoch column (i.e. post-task,pre-task and task). The last
+% %column is the file_path to these three epoch locations for this group, but i
+% %just want one of them as i will later be removing the last two files in
+% %the path to just have the path to the epochs.mat file two folders up. 
+% epochs_t.file_path = splitapply(@(x) x(1),t.path,groups);
+% 
+% %remove last two files in each file path (thus leading to epochs.mat)
+% epochs_t.file_path = cellfun(@remove_last_2_folders,epochs_t.file_path,'UniformOutput',false);
 
 %note: make the above code its own script later for better modularization
+load('Y:\Seth_temp\Thesis recordings\epochs_table.mat') %load struct 'epochs_t'
 
 %%
 %%collect data
