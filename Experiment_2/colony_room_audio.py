f83bd086d9de8df44aa2c04bb04400179ca4ff91
SethYC <seth.campbell@uleth.ca>
2022-09-09 18:04:55 -0600

    add colony room audio python script

---
+++ Experiment_2/colony_room_audio.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Script to be used on a PC in a rat colony room to play the bird chirping audio intermittently for 5 hours when specified.
+
+Created on Fri Sep  9 17:58:50 2022
+
+@author: seth.campbell
+"""
+
