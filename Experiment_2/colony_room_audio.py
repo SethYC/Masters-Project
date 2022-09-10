94aa521bf97c3f36026fa38ab6759dc82d7813be
SethYC <seth.campbell@uleth.ca>
2022-09-09 19:40:36 -0600

    change var names

--- Experiment_2/colony_room_audio.py
+++ Experiment_2/colony_room_audio.py
@@ -10,12 +10,12 @@ Created on Fri Sep  9 17:58:50 2022
 #init constants/parameters 
 path = '/home/seth/Downloads/foo2.wav' #path to sound file
 path2 = '/home/seth/Downloads/The sound of the night_ cicadas and nocturnal birds.mp3'
-duration = 5 #hours
+duration_hr = 5 #hours
 
 #import libraries
 import pygame
 
-duration = duration*60*60 #convert hours to seconds
+duration_s = duration_hr*60*60 #convert hours to seconds
 
 pygame.init()
 chirp = pygame.mixer.music
