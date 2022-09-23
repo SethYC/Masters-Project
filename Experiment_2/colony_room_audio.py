74195deb3b1f34f936b841f6b28fb1abb954bdac
SethYC <seth.campbell@uleth.ca>
2022-09-22 19:00:30 -0600

    start automated playback control

--- Experiment_2/colony_room_audio.py
+++ Experiment_2/colony_room_audio.py
@@ -11,9 +11,12 @@ Created on Fri Sep  9 17:58:50 2022
 path = '/home/seth/Downloads/foo2.wav' #path to sound file
 path2 = '/home/seth/Downloads/The sound of the night_ cicadas and nocturnal birds.mp3'
 duration_hr = 5 #hours
+bin_dur = 10 #seconds
+sound_on_ratio = 0.6 
 
 #import libraries
 import pygame
+import time
 from datetime import datetime
 
 duration_s = duration_hr*60*60 #convert hours to seconds
@@ -27,6 +30,9 @@ chirp.load(path2) #init music stream
 chirp.play(loops=-1) #start infinite loop music stream 
 
 while True:
+    time.sleep(bin_dur)
+    #generate rand int from 0 to 1, if larger than sound_on_ration, then
+    #stop audio, else play audio
     elapsed_t = (datetime.now()-start_t).total_seconds()
     if elapsed_t >= duration_s:
         print("time has elapsed")
