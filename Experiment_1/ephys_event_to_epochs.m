d8a0e9bf27ef4eaa6584b5d07ba2336271e6accb
SethYC <seth.campbell@uleth.ca>
2023-03-02 23:50:45 -0700

    finish SWR detection integration

--- Experiment_1/ephys_event_to_epochs.m
+++ Experiment_1/ephys_event_to_epochs.m
@@ -33,11 +33,20 @@ for i = 1:length(t.rat_num) %for each epoch file in all rats & days
             epochs.tsspin2 = ts;
         end
     else %response == 2 %SWRs
-        continue %placeholder
+%         continue %placeholder
+        hpc_ch = get_hpc_ch(t.group(i),t.rat_num(i));
+        ts = get_swrs(t.path{i},t.epoch(i),epochs,hpc_ch);
+
+        %save timestamps to epochs.mat
+        if t.epoch(i) == 'pre-task_sleep'
+            epochs.tsswr1 = ts;
+        else %post-task_sleep
+            epochs.tsswr2 = ts;
+        end
     end
 
     %save epochs.mat
-%     save([epochs_path,'\epochs.mat'],'epochs')
+    save([epochs_path,'\epochs.mat'],'epochs')
     
     fprintf("progress: %i/%i\n", i , length(t.rat_num))
 end
@@ -78,7 +87,68 @@ function ts = get_spindles(path,epoch_name,epochs,ctx_ch)
     diffs = ts(:,2)-ts(:,1); %difference between end and start times
     ts(diffs>2e4,:) = []; %1e4 nsma units = 1 sec
 
+end
+
+function ts = get_swrs(path,epoch_name,epochs,hpc_ch)
+
+    if epoch_name == 'pre-task_sleep'
+        eeg_tsd = csc2tsd_badclock([path, '\', hpc_ch],epochs.sleep1);
+    else % 'post-task_sleep'
+        eeg_tsd = csc2tsd_badclock([path, '\', hpc_ch],epochs.sleep2);
+    end
+
+    %preprocessing
+    eeg_tsd = downsamp(eeg_tsd,2000); %downsample to 2kHz
+    ts_range = Range(eeg_tsd,'sec'); %get timestamps, but in seconds
+    data = Data(eeg_tsd);
+
+    %bandstop filter between 178-182Hz to remove 180Hz (based on 60Hz harmonic)
+    %noise in SWR freq. range (1-3kHz). Power spectral density plots in
+    %testing did not show any other noticable spikes in noise due to 60Hz
+    %harmonics. 
+    data = bandstop(data,[178 182],2000); 
+
+    %find SWRs with Mike and Karim's function
+    if epoch_name == 'pre-task_sleep'
+        [ts, ~] = ripple_detect(ts_range, data, (epochs.tssleep1 ./ 1e4), 7, 1);
+    else
+        [ts, ~] = ripple_detect(ts_range, data, (epochs.tssleep2 ./ 1e4), 7, 1);
+    end
     
-    
+%     %remove swrs outside of motionlessness
+%     if epoch_name == 'post-task_sleep'
+%         ts = rm_ts_overlap(ts,epochs.tswake2);
+%     else %pre-task_sleep
+%         ts = rm_ts_overlap(ts,epochs.tswake1);
+%     end
+
+    %remove SWRs longer than 100ms
+    swr_durations = ts(:,2)-ts(:,1);
+    ts(swr_durations>0.1,:) = [];
+
+    %convert sec to nsma 
+    ts = ts*1e4;
+
+end
+
+function hpc_ch = get_hpc_ch(group,rat_num)
+
+    if group == 'C'
+        if rat_num == 1
+            hpc_ch = 'hpc4.ncs';
+        else % rat_num == 2
+            hpc_ch = 'hpc1.ncs';
+        end
+    else %group == 'E'
+        if rat_num == 1
+            hpc_ch = 'hpc4.ncs';
+        elseif rat_num == 2
+            hpc_ch = 'hpc2.ncs';   
+        elseif rat_num == 3
+            hpc_ch = 'hpc2.ncs';
+        else %rat_num == 4
+            hpc_ch = 'hpc1.ncs';
+        end
+    end
 
 end
